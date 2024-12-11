0bbb8924f19c22b49577dc459355040c92faf0aa
reako99 <zldrgx@naver.com>
2024-12-11 21:07:41 +0900

    add provider.java

---
+++ board-back/src/main/java/com/bjc/board_back/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.bjc.board_back.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.bjc.board_back.provider.JwtProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+    
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+    }
+
+    private String parseBearerToken(HttpServletRequest request){
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if (!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("bearer");
+        if (!isBearer) return null;
+
+        String token = authorization.substring(7);
+
+        return  token;
+    }
+}
