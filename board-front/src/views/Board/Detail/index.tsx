9547d21e9931de0e7025348472e2e4f3a5cb3cb6
reako99 <zldrgx@naver.com>
2024-12-29 01:19:33 +0900

    update board detail event handler (front)

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './style.css';
 import FavoriteItem from 'components/FavoriteItem';
 import { Board, CommentListItem, FavoriteListItem } from 'types/interface';
@@ -100,48 +100,105 @@ export default function BoardDetail() {
   //          component: 게시물 상세 하단 컴포넌트          //
   const BoardDetailBottom = () => {
 
+    //           state: comment textarea ref state          //
+    const commentRef = useRef<HTMLTextAreaElement | null >(null);
+
+    //           state: favorite state           //
+    const [isFavorite, setFavorite] = useState<boolean>(false);
+    //           state: show favorite state           //
+    const [showFavorite, setShowFavorite] = useState<boolean>(false);
+    //           state: show comment state           //
+    const [showComment, setShowComment] = useState<boolean>(false);
+
+    //           state: favorite list state           //
     const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>([]);
+    //           state: comment list state           //
     const [commentList, setCommentList] = useState<CommentListItem[]>([]);
 
+    //           state: comment state           //
+    const [comment, setComment] = useState<string>('');
+
+    //           event handler: favorite click event handler          //
+    const onFavoriteClickHandler = () => {
+      setFavorite(!isFavorite);
+    }
+
+    //           event handler: show favorite click event handler          //
+    const onShowFavoriteClickHandler = () => {
+      setShowFavorite(!showFavorite);
+    }
+
+    //           event handler: show comment click event handler          //
+    const onShowCommentClickHandler = () => {
+      setShowComment(!showComment);
+    }
+
+    //           event handler: comment submit button click event handler          //
+    const onCommentSubmitButtonClickHandler = () => {
+      if (comment.trim()==='') return;
+    }
+
+    //           event handler: comment change event handler          //
+    const onCommentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      const { value } = event.target;
+      setComment(value);
+      if(!commentRef.current) return;
+      commentRef.current.style.height = 'auto';
+      commentRef.current.style.height = `${commentRef.current.scrollHeight}px`;
+    }
+
+    //          effect: reload favorite and comment list when change board number path variable          //
     useEffect(() => {
       setFavoriteList(favoriteListMock);
       setCommentList(commentListMock);
-    }, []);
+    }, [boardNumber]);
 
     //         render : 게시물 상세 하단 컴포넌트 랜더링          //
     return (
       <div id='board-detail-bottom'>
         <div className='board-detail-bottom-button-box'>
           <div className='board-detail-bottom-button-group'>
-            <div className='icon-button'>
-              <div className='icon favorite-fill-icon'></div>
+            <div className='icon-button' onClick={onFavoriteClickHandler}>
+              {isFavorite?
+              <div className='icon favorite-fill-icon'></div> :
+              <div className='icon favorite-light-icon'></div>
+              }              
             </div>
-            <div className='board-detail-bottom-button-text'>{`좋아요 ${12}`}</div>
-            <div className='icon-button'>
-              <div className='icon up-light-icon'></div>
+            <div className='board-detail-bottom-button-text'>{`좋아요 ${favoriteList.length}`}</div>
+            <div className='icon-button' onClick={onShowFavoriteClickHandler}>
+              {showFavorite ? 
+              <div className='icon up-light-icon'></div> :
+              <div className='icon down-light-icon'></div>
+              }
             </div>
           </div>
           <div className='board-detail-bottom-button-group'>
             <div className='icon-button'>
               <div className='icon comment-icon'></div>
             </div>
-            <div className='board-detail-bottom-button-text'>{`댓글 ${11}`}</div>
-            <div className='icon-button'>
-              <div className='icon up-light-icon'></div>
+            <div className='board-detail-bottom-button-text'>{`댓글 ${commentList.length}`}</div>
+            <div className='icon-button' onClick={onShowCommentClickHandler}>
+              {showComment ? 
+              <div className='icon up-light-icon'></div> :
+              <div className='icon down-light-icon'></div>
+              }
             </div>
           </div>
         </div>
+        {showFavorite && 
         <div className='board-detail-bottom-favorite-box'>
           <div className='board-detail-bottom-favorite-container'>
-            <div className='board-detail-bottom-favorite-title'>{'좋아요 '}<span className='emphasis'>{12}</span></div>
+            <div className='board-detail-bottom-favorite-title'>{'좋아요 '}<span className='emphasis'>{favoriteList.length}</span></div>
             <div className='board-detail-bottom-favorite-contents'>
               { favoriteList.map(item => <FavoriteItem favoriteListItem={item} />)}
             </div>
           </div>
         </div>
+        }
+        {showComment &&
         <div className='board-detail-bottom-comment-box'>
           <div className='board-detail-bottom-comment-container'>
-            <div className='board-detail-bottom-comment-title'>{'댓글 '}<span className='emphasis'>{11}</span></div>
+            <div className='board-detail-bottom-comment-title'>{'댓글 '}<span className='emphasis'>{commentList.length}</span></div>
             <div className='board-detail-bottom-comment-list-container'>
               { commentList.map(item => <CommentItem commentListItem={item} />)}
             </div>
@@ -152,13 +209,14 @@ export default function BoardDetail() {
           </div>
           <div className='board-detail-bottom-comment-input-box'>
             <div className='board-detail-bottom-comment-input-container'>
-              <textarea className='board-detail-bottom-comment-textarea' placeholder='댓글을 작성해주세요.' />
+              <textarea ref={commentRef} className='board-detail-bottom-comment-textarea' placeholder='댓글을 작성해주세요.' onChange={onCommentChangeHandler} value={comment} />
               <div className='board-detail-bottom-comment-button-box'>
-                <div className='disable-button'>{'댓글달기'}</div>
+                <div className={comment.trim() === '' ? 'disable-button' : 'black-button' } onClick={onCommentSubmitButtonClickHandler}>{'댓글달기'}</div>
               </div>
             </div>
           </div>
         </div>
+        }
       </div>
     );
   }
