f856dde826acf1761285c403a27c978774352a62
reako99 <137850852+reako99@users.noreply.github.com>
2024-12-31 18:56:44 +0900

    Update BoardController.java

--- board-back/src/main/java/com/bjc/board_back/controller/BoardController.java
+++ board-back/src/main/java/com/bjc/board_back/controller/BoardController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bjc.board_back.dto.request.board.PostBoardRequestDto;
 import com.bjc.board_back.dto.request.board.PostCommentRequestDto;
 import com.bjc.board_back.dto.response.board.DeleteBoardResponseDto;
