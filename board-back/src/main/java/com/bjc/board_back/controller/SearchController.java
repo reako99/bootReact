080511800fd8c34f413a3c4f4faa1e2b3b765a16
reako99 <137850852+reako99@users.noreply.github.com>
2025-01-08 22:05:35 +0900

    SearchController.java 업데이트

--- board-back/src/main/java/com/bjc/board_back/controller/SearchController.java
+++ board-back/src/main/java/com/bjc/board_back/controller/SearchController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bjc.board_back.dto.response.search.GetPopularListResponseDto;
 import com.bjc.board_back.dto.response.search.GetRelationListResponseDto;
 import com.bjc.board_back.service.SearchService;
